57fd49bd6ce3be7fbdfb72e8e704b2c011fd2bcc
Sapna Devi <saroasapna4@gmail.com>
2025-06-26 11:06:47 +0530

    this is changed repository

--- app/MainActivity.kt
+++ app/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etUsername : EditText
-    lateinit var etEmail : EditText
-    lateinit var etPassword : EditText
+    lateinit var etusername : EditText
+    lateinit var etemail : EditText
+    lateinit var etpassword : EditText
     lateinit var btnCheck : Button
+
+    lateinit var tvEmail : TextView
+    lateinit var tvPassword: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,22 +24,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        etUsername = findViewById(R.id.etUsername)
-        etEmail = findViewById(R.id.etEmail)
-        etPassword = findViewById(R.id.etPassword)
+        username = findViewById(R.id.etUsername)
+        email = findViewById(R.id.etEmail)
+        password = findViewById(R.id.etPassword)
         btnCheck = findViewById(R.id.btnCheck)
-
         btnCheck.setOnClickListener {
-            if(etUsername.text.isEmpty()){//true
-                etUsername.error = "Enter Your Username"
-            } else if(etEmail.text.isEmpty()){
-                etPassword.error = "Enter Your Email"
-            else if(etPassword.text.isEmpty()){
-                etPassword.error = "Enter Your Password"
+            if(username.text.isEmpty()){//true
+                username.error = "Enter Your Username"
+            } else if(email.text.isEmpty()){
+                email.error = "Enter Your Email"
+            else if(password.text.isEmpty()){
+                password.error = "Enter Your Password"
             }else{
-                println(" Username: ${etUsername.text}")
-                println(" Email: ${etemail.text}")
-                    println("Password: ${etPassword.text}")
+                    tvEmail.text = email.text.toString()
+                    tvPassword.text = password.text.toString()
+                }
                     Toast.makeText(this, "Sign Up Successfully!", Toast.LENGTH_SHORT).show()
 
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "pratise android"
 include(":app")
+include(":app")
  
